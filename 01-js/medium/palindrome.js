525b963e40d53364c0d69f91156d45dcb84f2c56
abhinav kumar <abhinav.mg.k@gmail.com>
2025-03-04 20:06:30 +0530

    medium solved

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -4,13 +4,13 @@
 */
 
 function isPalindrome(str) {
-  
+
+  str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    
+  return str === str.split('').reverse().join('');
 
 
 
-
-
-  return true;
 }
 
 module.exports = isPalindrome;
